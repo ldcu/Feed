f7a7cddcd23f246012931f35d95918b824b3b07b
ldcu <udqq@icloud.com>
2021-02-17 02:01:18 +0000

    c2511e98dd3f658dcb5ed167b81c5c6f

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,192 +1,241 @@
-import Axios from "axios";
 import React from "react";
-import ReactPaginate from "react-paginate";
 import { Container } from "react-bootstrap";
 import { Helmet } from "react-helmet";
 import Layout from "../css/layout.module.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
-import Cookies from "js-cookie";
+import { Pagination } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const processString = require("react-process-string"); // Used for processing the string.
 
 let clickableLink = [
 	{
-		// 'clickableLink' for identifying links
+		// This is for making the domains (ex.: example.com) clickable. Otherwise you can't click on a link from a text.
 		regex: /(http|https):\/\/(\S+)\.([a-z]{2,}?)(.*?)( |,|$|\.|\))/gim, // This is for link starting with 'http' or 'https'.
 		fn: (key, result) => (
-			<span key={key}>
-				<a target="_blank" rel="noopener noreferrer" className="link" href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`} >{" "}{result[2]}.{result[3]}{result[4]}</a>{" "}{result[5]}
-			</span>
+			<span key={key}><a target="_blank" rel="noopener noreferrer" className="link" href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`}>{" "}{result[2]}.{result[3]}{result[4]}</a>{" "}{result[5]}</span>
 		),
 	},
 	{
 		regex: /(\S+)\.([a-z]{2,}?)(.*?)( |,|$|\.|\))/gim, // This is for any word that ends in .com or .something, and starts with anything. Meaning it will turn it into a link.
 		fn: (key, result) => (
-			<span key={key}>
-				<a target="_blank" rel="noopener noreferrer" className="link" href={`http://${result[1]}.${result[2]}${result[3]}`}>{" "}{result[1]}.{result[2]}{result[3]}</a>{" "}{result[4]}
-			</span>
+			<span key={key}><a target="_blank" rel="noopener noreferrer" className="link" href={`http://${result[1]}.${result[2]}${result[3]}`}>{" "}{result[1]}.{result[2]}{result[3]}</a>{" "}{result[4]}</span>
 		),
 	},
 ];
 
+const PaginationPage = (props) => {
+	const pageLinks = [];
+
+	let start = props.currentPage - (props.currentPage % 10);
+	if (start <= 0) start = 1;
+	for (let i = start; i <= start + 11 && i <= props.pages; i++) {
+		pageLinks.push(
+			<Pagination.Item>
+				<Link className="link" onClick={() => props.nextPage(i)}>
+					{i}
+				</Link>
+			</Pagination.Item>
+		);
+	}
+
+	return (
+		<Pagination size="sm" className="customPagination">
+			{props.currentPage > 10 && (
+				<Pagination.Item>
+					<Link className="link" onClick={() => props.tenChange(props.currentPage, -1)}>- 10</Link>
+				</Pagination.Item>
+			)}
+			{pageLinks}
+			{props.currentPage + 10 < props.pages && (
+				<Pagination.Item>
+					<Link className="link" onClick={() => props.tenChange(props.currentPage, 1)}>+ 10</Link>
+				</Pagination.Item>
+			)}
+		</Pagination>
+	);
+};
+
 const headers = {
-	headers: { Authorization: `Bearer ${localStorage.getItem("access_token")}` }
-}
+	headers: { Authorization: `Bearer ${localStorage.getItem("access_token")}` },
+};
 
 class Feed extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			offset: 0,
 			data: [],
-			perPage: 10, // 10 items per page to be returned.
-			currentPage: 0,
-			googleId: Cookies.get("googleId"),
+			currentPage: 1,
+			totalFeed: 0,
 		};
-		this.handlePageClick = this.handlePageClick.bind(this);
 	}
 
-	receivedData() {
-		Axios.get(`/api/feed`, headers).then((res) => {
-			const data = res.data;
-			const slice = data.slice(
-				this.state.offset,
-				this.state.offset + this.state.perPage
-			);
-			const postData = slice.map((
-				fields // Slicing the data so we can have pagination.
-			) => (
-				<React.Fragment key={fields._id}>
-					<ListGroup >
-						<ListGroup.Item
-							style={{
-								backgroundColor: "#121212",
-								color: "#b7b7b7",
-								border: "none",
-							}}
-						>
-							{processString(clickableLink)(fields.content)}
-							<br />
-							<hr className="half-rule" />
-							<div align="right">
-								<small className="text-muted">{formatDate(fields.date)}</small>
-							</div>
-						</ListGroup.Item>
-					</ListGroup>
-					<br />
-				</React.Fragment>
-			));
+	nextpage = (pageNumber) => {
+		this.setState({
+			currentPage: pageNumber,
+			data: [],
+		});
+		this.getUsers(pageNumber);
+	};
 
-			this.setState({
-				pageCount: Math.ceil(data.length / this.state.perPage), // Making the pages.
+	tenChange = (pageNumber, isposOrneg) => {
+		var finalPage;
+		if (isposOrneg > 0)
+			//+10 clicked
+			finalPage = pageNumber + 10;
+		//-10 clicked
+		else finalPage = pageNumber - 10;
+		this.setState({
+			currentPage: finalPage,
+			data: [],
+		});
+		this.getUsers(finalPage);
+	};
 
-				postData,
-			});
-		}).catch(console.log);
-	}
+	hundreadChange = (pageNumber, isposOrneg) => {
+		var finalPage;
+		if (isposOrneg > 0)
+			//+100 clicked
+			finalPage = pageNumber + 100;
+		//-100 Clicked
+		else finalPage = pageNumber - 100;
+		this.setState({
+			currentPage: finalPage,
+			data: [],
+		});
+		this.getUsers(finalPage);
+	};
 
-	handlePageClick = (e) => {
-		const selectedPage = e.selected;
-		const offset = selectedPage * this.state.perPage;
-
-		this.setState(
-			{
-				currentPage: selectedPage,
-				offset: offset,
+	dataRequest = (URL, methodType, params) => {
+		return fetch(URL, {
+			method: methodType,
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
 			},
-			() => {
-				this.receivedData();
-			}
-		);
+			body: JSON.stringify(params),
+		})
+			.then((data) => {
+				return data.json();
+			})
+			.catch((err) => {
+				return err;
+			});
+	};
+
+	getUsers = (currentPage) => {
+		console.log("Rendering Inside GetUsers");
+		const queryParams = {};
+		queryParams["page"] = currentPage; //Page Number
+		queryParams["pagination"] = 8; //Number Of records on Page
+		this.dataRequest("/api/feed", "POST", queryParams)
+			.then((data) => {
+				console.log("Data FEtched ", data);
+				this.setState({
+					data: data.users,
+				});
+			})
+			.catch((err) => {
+				console.log("Error In Fetching Users ", err);
+			});
+	};
+
+	getUsersCount = () => {
+		//Passing /1 as Backend Uses same query so if argument then it will return count
+		this.dataRequest("/api/feed/1", "GET").then((data) => {
+			this.setState(
+				{
+					totalFeed: data.total,
+				}, //call is for first page records only
+				() => this.getUsers(this.state.currentPage)
+			);
+		});
 	};
 
 	componentDidMount() {
-		this.receivedData();
+		this.getUsersCount();
 	}
 
 	render() {
+		let numberOfPages = 0;
+		if (this.state.totalFeed % 8 === 0)
+			numberOfPages = Math.floor(this.state.totalFeed / 8);
+		else numberOfPages = Math.floor(this.state.totalFeed / 8) + 1;
 
 		return (
 			<>
-			<div className={Layout.container}>
-				<Helmet>
-					<title>Feed</title>
-				</Helmet>
-				<Container>
-					<h1 style={{ color: "#b7b7b7" }}>Feed</h1>
-					<br />
-
-					<form
-						id="feed-form"
-						onSubmit={this.handleSubmit.bind(this)}
-						method="POST"
-						elevation={0}
-					>
-						<Form.Control
-							as="textarea"
-							rows="10"
-							name="feed"
-							style={{
-								backgroundColor: "#121212",
-								color: "#b7b7b7",
-								boxShadow: "0px 0px 0px white",
-								border: "none",
-							}}
-							elevation={0}
-							value={this.state.content}
-							onChange={this.onMessageChange.bind(this)}
-							className="block"
-							placeholder="What's on your mind?"
-						/>
+				<div className={Layout.container}>
+					<Helmet>
+						<title>Feed</title>
+					</Helmet>
+					<Container>
+						<h1 style={{ color: "#b7b7b7" }}>Feed</h1>
 						<br />
 
-						<div align="right">
-							<Button
-								type="submit"
-								size="lg"
-								style={{
-									border: "none",
-									boxShadow: "0px 0px 0px white",
-									backgroundColor: "#121212",
-									color: "#b7b7b7",
-								}}
-							>
-								Send
-							</Button>
-						</div>
-						<br />
-					</form>
+						<form id="feed-form" onSubmit={this.handleSubmit.bind(this)} method="POST" elevation={0}>
+							<Form.Control
+								as="textarea"
+								rows="10"
+								name="feed"
+								style={{ backgroundColor: "#121212", color: "#b7b7b7", boxShadow: "0px 0px 0px white", border: "none",}}
+								elevation={0}
+								value={this.state.content}
+								onChange={this.onMessageChange.bind(this)}
+								className="block"
+								placeholder="What's on your mind?"
+							/>
+							<br />
 
-					<div>
-						<ReactPaginate
-							previousLabel={"←"}
-							nextLabel={"→"}
-							breakLabel={"..."}
-							breakClassName={"break-me"}
-							pageCount={this.state.pageCount}
-							marginPagesDisplayed={2}
-							pageRangeDisplayed={5}
-							onPageChange={this.handlePageClick}
-							containerClassName={"pagination"}
-							subContainerClassName={"pages pagination"}
-							activeClassName={"active"}
-						/>
+							<div align="right">
+								<Button type="submit" size="lg" style={{ border: "none", boxShadow: "0px 0px 0px white", backgroundColor: "#121212", color: "#b7b7b7", }}>Send</Button>
+							</div>
+							<br />
+						</form>
 
-						{this.state.postData}
-					</div>
-				</Container>
-			</div>
+						{this.state.totalFeed > 8 && (
+							<PaginationPage
+								pages={numberOfPages}
+								nextPage={this.nextpage}
+								currentPage={this.state.currentPage}
+								tenChange={this.tenChange}
+								hundreadChange={this.hundreadChange}
+							></PaginationPage>
+						)}
 
-			{/* Back to home button. */}
+						{this.state.data.map((fields) => {
+							const { _id, content, date } = fields;
+							return (
+								<React.Fragment key={_id}>
+									<ListGroup>
+										<ListGroup.Item style={{ backgroundColor: "#121212", color: "#b7b7b7", border: "none", }} >
+											{processString(clickableLink)(content)}
+											<br />
+											<hr className="half-rule" />
+											<div align="right">
+												<small className="text-muted">
+													{formatDate(fields.date)}
+												</small>
+											</div>
+										</ListGroup.Item>
+									</ListGroup>
+									<br />
+								</React.Fragment>
+							);
+						})}
+					</Container>
+				</div>
 
-			<hr className="half-rule" />
-			<a href="/home" className="link">
-				← Back to home
-			</a>
-		</>
-		)
+				{/* Back to home button. */}
+
+				<hr className="half-rule" />
+				<a href="/home" className="link">
+					← Back to home
+				</a>
+			</>
+		);
 	}
 
 	onMessageChange(event) {
@@ -195,16 +244,14 @@ class Feed extends React.Component {
 
 	handleSubmit(e) {
 		e.preventDefault();
-		
-		fetch(
-			"/api/feed",
-			{
+
+		fetch("/api/feed/post", {
 			method: "POST",
 			body: JSON.stringify(this.state),
 			headers: {
 				Accept: "application/json",
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${localStorage.getItem("access_token")}`
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
 			},
 		})
 			.then((response) => response.json())
@@ -217,29 +264,7 @@ class Feed extends React.Component {
 				} else if (response.error) {
 					alert("Message failed to send."); // Pop-up for when it fails to send the message.
 				}
-			})
-
-		// Axios.post(
-		// 	"/api/feed",
-		// 	this.state.content,
-		// 	headers
-		// ).then((response) => response.json())
-		// .then((response) => {
-		// 	if (!response.error) {
-		// 		// alert("Message Sent."); // Pop-up that let's you know the content was successfully submitted.
-		// 		this.setState({ content: "" });
-		// 		// window.location.reload() // Refresh page.
-		// 		this.componentDidMount(); // Refresh component after sending the content so you'd have the latest record displaying on the page.
-		// 	} else if (response.error) {
-		// 		alert("Message failed to send."); // Pop-up for when it fails to send the message.
-		// 	}
-		// })
-		// .catch(function(error) {
-		// 	console.log("Tough luck. \"" + error + "\"");
-		// })
-			
-			
-			;
+			});
 	}
 }
 
--- routes/feed-routes.js
+++ routes/feed-routes.js
@@ -1,48 +1,77 @@
 const mongoose = require("mongoose");
 const Feed = mongoose.model("feed");
-const checkAuth = require('../middleware/check-auth'); // checkAuth function is for making the API to work only with an "access_token".
+const checkAuth = require("../middleware/check-auth"); // checkAuth function is for making the API to work only with an "access_token".
 
 module.exports = (app) => {
-	app.get(`/api/feed`, checkAuth, async (req, res) => {
-		let feed = await Feed.find().sort({ date: -1 });
-		return res.status(200).send(feed);
-	});
-
-	app.get(`/api/feed/count`, checkAuth, async (req, res) => {
-		let feed = await Feed.estimatedDocumentCount();
-		return res.status(200).send({
-			error: false,
-			feed,
+  app.get("/api/feed/:id?", checkAuth, function (req, res) {
+	//Pagination For number Of receords on page
+	if (req.params.id) {
+	  //Case For Counting Number OF Users
+	  Feed.find({})
+		.count()
+		.then((data) => {
+		  res.status(200).send({
+			total: data,
+		  });
+		})
+		.catch((err) => {
+		  res.status(400).send({
+			err: err,
+		  });
 		});
-	});
+	}
+  });
 
-	app.post(`/api/feed`, checkAuth, async (req, res) => {
-		let feed = await Feed.create(req.body);
-		return res.status(201).send({
-			error: false,
-			feed,
+  app.post("/api/feed/", checkAuth, function (req, res) {
+	const pagination = req.body.pagination ? parseInt(req.body.pagination) : 10;
+	//PageNumber From which Page to Start
+	const pageNumber = req.body.page ? parseInt(req.body.page) : 1;
+	Feed.find({})
+	  //skip takes argument to skip number of entries
+	  .sort({ id: 1 })
+	  .skip((pageNumber - 1) * pagination)
+	  //limit is number of Records we want to display
+	  .limit(pagination)
+	  .sort({ date: -1 })
+	  .then((data) => {
+		res.status(200).send({
+		  users: data,
 		});
-	});
-
-	app.put(`/api/feed/:id`, checkAuth, async (req, res) => {
-		const { id } = req.params;
-
-		let feed = await Feed.findByIdAndUpdate(id, req.body);
-
-		return res.status(202).send({
-			error: false,
-			feed,
+	  })
+	  .catch((err) => {
+		res.status(400).send({
+		  err: err,
 		});
+	  });
+  });
+
+  app.post(`/api/feed/post`, checkAuth, async (req, res) => {
+	let feed = await Feed.create(req.body);
+	return res.status(201).send({
+	  error: false,
+	  feed,
 	});
+  });
 
-	app.delete(`/api/feed/:id`, checkAuth, async (req, res) => {
-		const { id } = req.params;
+  app.put(`/api/feed/:id`, checkAuth, async (req, res) => {
+	const { id } = req.params;
 
-		let feed = await Feed.findByIdAndDelete(id);
+	let feed = await Feed.findByIdAndUpdate(id, req.body);
 
-		return res.status(202).send({
-			error: false,
-			feed,
-		});
+	return res.status(202).send({
+	  error: false,
+	  feed,
 	});
+  });
+
+  app.delete(`/api/feed/:id`, checkAuth, async (req, res) => {
+	const { id } = req.params;
+
+	let feed = await Feed.findByIdAndDelete(id);
+
+	return res.status(202).send({
+	  error: false,
+	  feed,
+	});
+  });
 };
